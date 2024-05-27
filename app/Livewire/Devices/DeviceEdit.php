8fc2bcf921a789854bc94dd578f9cd389209b5d7
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-27 16:23:08 +0700

    added Device Edit page & components

--- app/Livewire/Devices/DeviceEdit.php
+++ app/Livewire/Devices/DeviceEdit.php
@@ -2,12 +2,59 @@
 
 namespace App\Livewire\Devices;
 
+use Carbon\Carbon;
+use App\Models\Device;
 use Livewire\Component;
+use App\Models\DeviceName;
 
 class DeviceEdit extends Component
 {
+    public $alat, $namaAlat, $deviceId, $nama, $merk, $tipe, $serial_number, $lokasi, $rumah_sakit, $kalibrasi_terakhir, $status;
+    public function mount($deviceId)
+    {
+        $this->alat = Device::where('deviceId', $deviceId)->first();
+        $this->namaAlat = DeviceName::all();
+        $this->nama = $this->alat->name_id;
+        $this->merk = $this->alat->brand;
+        $this->tipe = $this->alat->type;
+        $this->serial_number = $this->alat->serial_number;
+        $this->lokasi = $this->alat->location;
+        $this->rumah_sakit = $this->alat->hospital_id;
+        $this->kalibrasi_terakhir = $this->alat->calibration_date;
+        $this->status = $this->alat->status;
+    }
+
+    public function update()
+    {
+        Device::where('deviceId', $this->deviceId)->update([
+            'name_id' => $this->nama,
+            'brand' => $this->merk,
+            'type' => $this->tipe,
+            'serial_number' => $this->serial_number,
+            'location' => $this->lokasi,
+            'hospital_id' => $this->rumah_sakit,
+            'calibration_date' => $this->kalibrasi_terakhir,
+            'next_calibration_date' => Carbon::parse($this->kalibrasi_terakhir)->addYear(),
+            'status' => $this->status,
+            'user_id' => auth()->user()->id
+        ]);
+        session()->flash('alert', [
+            'type' => 'success',
+            'title' => 'Alat berhasil diperbarui!',
+            'toast'=> true,
+            'position'=> 'top-end',
+            'timer'=> 3000,
+            'progbar' => true,
+            'showConfirmButton'=> false
+        ]);
+        return $this->redirectRoute('devices.index', navigate:true);
+
+    }
     public function render()
     {
-        return view('livewire.devices.device-edit');
+        return view('livewire.devices.device-edit', [
+            'qr' => $this->alat,
+            'name' => $this->namaAlat
+        ]);
     }
 }
--- resources/views/livewire/devices/device-edit.blade.php
+++ resources/views/livewire/devices/device-edit.blade.php
@@ -1,3 +1,73 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <div class="py-4 main">
+        <div class="row">
+            <div class="col-12 col-xl-12">
+                <div class="px-0 col-12">
+                    <div class="border-0 shadow card">
+                        <div class="card-body">
+                            <h2 class="mb-1 fs-5 fw-bold mb-5">{{ __('Update QR Alat') }}</h2>
+                            <div class="row mb-5">
+                                <div class="col">
+                                    <a href="{{ route('devices.index') }}" wire:navigate class="btn btn-primary text-white"><i
+                                            class="fas fa-arrow-left"></i>
+                                        Kembali</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <form wire:submit='update' method="post">
+                                        <div class="form-group mb-3">
+                                            <label for="nama" class="form-label text-capitalize">{{ __('nama') }}</label>
+                                            <select type="text" class="form-control" wire:model='nama'>
+                                                <option value="">Pilih salah satu...</option>
+                                                @foreach ($name as $nama)
+                                                    <option value="{{ $nama->id }}" {{ old('name_id', $qr->name_id) == $nama->id ? 'selected' : '' }}>{{ $nama->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <label for="merk" class="form-label text-capitalize">{{ __('merk') }}</label>
+                                            <input type="text" class="form-control" wire:model='merk'>
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <label for="tipe" class="form-label text-capitalize">{{ __('tipe') }}</label>
+                                            <input type="text" class="form-control" wire:model='tipe'>
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <label for="serial_number" class="form-label text-capitalize">{{ __('serial number') }}</label>
+                                            <input type="text" class="form-control" wire:model='serial_number'>
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <label for="lokasi" class="form-label text-capitalize">{{ __('lokasi') }}</label>
+                                            <input type="text" class="form-control" wire:model='lokasi'>
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <label for="rumah_sakit" class="form-label text-capitalize">{{ __('rumah sakit') }}</label>
+                                            <input type="text" class="form-control" wire:model='rumah_sakit'>
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <label for="kalibrasi_terakhir" class="form-label text-capitalize">{{ __('kalibrasi terakhir') }}</label>
+                                            <input type="date" class="form-control" wire:model='kalibrasi_terakhir' value="{{ Carbon\Carbon::parse(old('calibration_date'))->format('j F Y') }}">
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <label for="status" class="form-label text-capitalize">{{ __('status') }}</label>
+                                            <select class="form-control" wire:model='status'>
+                                                <option value="">Pilih salah satu...</option>
+                                                <option value="Tidak Tersedia" {{ old('status', $qr->status) === 'Tidak Tersedia' ? 'selected' : '' }}>{{ __('Tidak Tersedia') }}</option>
+                                                <option value="Dipinjamkan" {{ old('status', $qr->status) === 'Dipinjamkan' ? 'selected' : '' }}>{{ __('Dipinjamkan') }}</option>
+                                                <option value="Tersedia" {{ old('status', $qr->status) === 'Tersedia' ? 'selected' : '' }}>{{ __('Tersedia') }}</option>
+                                            </select>
+                                        </div>
+                                        <div class="d-grid">
+                                            <button type="submit" class="btn btn-success text-white">{{ __('Submit') }}</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
