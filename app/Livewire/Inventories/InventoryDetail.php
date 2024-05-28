b5565dfcd8b5f377dd30854f5610cfaccacd17fa
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-28 08:32:57 +0700

    change folder name from single to plural

---
+++ app/Livewire/Inventories/InventoryDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Inventories;
+
+use Livewire\Component;
+
+class InventoryDetail extends Component
+{
+    public function render()
+    {
+        return view('livewire.inventories.inventory-detail');
+    }
+}
