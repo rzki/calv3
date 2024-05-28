49e48ef9497763dcc2d6b4dd4215307557447093
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-28 21:20:57 +0700

    added Hospitals CRUD components

---
+++ app/Livewire/Hospitals/HospitalEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Hospitals;
+
+use Livewire\Component;
+
+class HospitalEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.hospitals.hospital-edit');
+    }
+}
--- app/Livewire/Inventories/InventoryDetail.php
+++ app/Livewire/Inventories/InventoryDetail.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire\Inventories;
 
+use App\Models\LogBook;
 use Livewire\Component;
+use App\Models\Inventory;
 
 class InventoryDetail extends Component
 {
+    public $inventories, $inventoryId, $logbook;
+    public function mount($inventoryId)
+    {
+        $this->inventories = Inventory::where('inventoryId', $inventoryId)->first();
+        $this->logbook = LogBook::where('inventory_id', $this->inventories->id)->with('inventories')->get();
+    }
     public function render()
     {
-        return view('livewire.inventories.inventory-detail');
+        return view('livewire.inventories.inventory-detail',[
+            'invDetail' => $this->inventories,
+            'logDetail' => $this->logbook
+        ]);
     }
 }
