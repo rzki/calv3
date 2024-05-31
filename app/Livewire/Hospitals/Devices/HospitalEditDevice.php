9c93ddf91bc5a81cf6833d38589e3f2c2f8138ca
Rizky Dhani <rizkydhani15@gmail.com>
2024-05-31 09:35:36 +0700

    added hospital device edit component

---
+++ app/Livewire/Hospitals/Devices/HospitalEditDevice.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Hospitals\Devices;
+
+use Livewire\Component;
+
+class HospitalEditDevice extends Component
+{
+    public function render()
+    {
+        return view('livewire.hospitals.devices.device-edit');
+    }
+}
