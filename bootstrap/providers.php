b6298d559722ad20953af1ee3456943d5fe14493
Rizky Dhani <rizkydhani15@gmail.com>
2024-06-01 12:27:57 +0700

    added Excel Import feature

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     Milon\Barcode\BarcodeServiceProvider::class,
-
+    Maatwebsite\Excel\ExcelServiceProvider::class
 ];
--- config/app.php
+++ config/app.php
@@ -129,6 +129,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
         'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ])->toArray(),
 
 ];
