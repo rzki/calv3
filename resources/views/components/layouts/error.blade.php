be867c9096e5b35f6423524bf502115aaaa7eaa3
Rizky Dhani <rizkydhani15@gmail.com>
2024-06-10 14:50:07 +0700

    custom error pages

---
+++ resources/views/components/layouts/error.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title ?? 'Page Title' }} - Kalibrasi</title>
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/brands.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/regular.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/solid.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+</head>
+    <div class="content">
+        @yield('content')
+    </div>
+
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+<body>
+
+</body>
+
+</html>
